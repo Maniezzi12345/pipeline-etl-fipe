8f9c21900f3a2743b63f82376842f564ebf3ced5
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-07-19 15:40:48 -0300

    feat: conexao python-postgresql via dois containers docker com rede interna

--- load/carregar.py
+++ load/carregar.py
@@ -1,19 +1,35 @@
+import os
 import psycopg2
-import pandas as pd 
-from sqlalchemy import create_engine
+import pandas as pd
+from dotenv import load_dotenv
 
-def criar_engine():
-    engine = create_engine(
-        "postgresql+psycopg2://admin:admin123@localhost:5432/fipe"
+load_dotenv()
+
+def criar_conexao():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
-    return engine
+    return conn
 
-def salvar_dataframe(df,tabela,engine):
-    df.to_sql(
-        name=tabela,
-        con=engine,
-        df_exists="append",
-        Index=False
-    )
+def salvar_dataframe(df, tabela):
+    conn = criar_conexao()
+    cursor = conn.cursor()
 
-print(f"{len(df)} registros inseridos em {tabela}")
+    colunas = ", ".join(df.columns)
+    valores = ", ".join(["%s"] * len(df.columns))
+
+    for _, row in df.iterrows():
+        valores_linha = tuple(None if pd.isna(v) else v for v in row)
+        cursor.execute(
+            f"INSERT INTO {tabela} ({colunas}) VALUES ({valores})",
+            valores_linha
+        )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print(f"✅ {len(df)} registros inseridos em {tabela}")
